8cdc8556416672d152633f5c0006753b484adb54
Luinix <luizmarcelolh@gmail.com>
2024-12-17 21:33:00 -0300

    update: adição das funções de libs

--- libs/funcoes.c
+++ libs/funcoes.c
@@ -1,1 +1,17 @@
-//Arquivo para unificar todas as funções de conversão
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+// Inclui todos os outros cabeçalhos das funções
+#include "comprimento.c"
+#include "massa.c"
+#include "volume.c"
+#include "temperatura.c"
+#include "velocidade.c"
+#include "eletricidade.c"
+#include "area.c"
+#include "tempo.c"
+#include "armazenamento.c"
+
+// Você pode adicionar mais funções auxiliares aqui, se necessário
+
+#endif // FUNCOES_H
