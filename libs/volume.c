eb7af15a15ada3feb30e26168ed69123c924c670
DanielSantos08 <danfergatthi@gmail.com>
2024-12-18 11:59:14 -0300

    Implementação das funçoes de conversão de volume

--- libs/volume.c
+++ libs/volume.c
@@ -1,1 +1,32 @@
 //Arquivo para funções de conversão de volume
+#ifndef VOLUME_H
+#define VOLUME_H
+
+// Conversões de Litros 
+float litrosToMililitros(float l) {
+    return l * 1000;
+}
+
+float litrosToMetrosCubicos(float l) {
+    return l / 1000;
+}
+
+// Conversões de Mililitros
+float mililitrosToLitros(float ml) {
+    return ml / 1000;
+}
+
+float mililitrosToMetrosCubicos(float ml) {
+    return ml / 1e6;
+}
+
+// Conversões de Metros Cúbicos
+float metrosCubicosToLitros(float m3) {
+    return m3 * 1000;
+}
+
+float metrosCubicosToMililitros(float m3) {
+    return m3 * 1e6;
+}
+
+#endif
