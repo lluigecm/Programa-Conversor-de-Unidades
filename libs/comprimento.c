b7f93505f834af3ac825639fcd80a939ba1d300a
Evelyn Suzarte <evelynsuzarte@hotmail.com>
2024-12-19 21:01:05 -0300

    criação de funcções de comprimento

--- libs/comprimento.c
+++ libs/comprimento.c
@@ -1,1 +1,32 @@
 //Arquivo para funções de conversão de comprimento
+#include <stdio.h>
+
+//metros para centimetros
+float metros_centimetros (float metros){
+    return metros * 100;
+}
+
+//metros para milimetros
+float metros_milimetros (float metros){
+    return metros * 1000;
+}
+
+// centimentros para metros
+float centimetros_metros (float centimetros){
+    return centimetros/100;
+}
+
+// centimentros para milimetros
+float centimetros_milimetros (float centimetros){
+    return centimetros*10;
+}
+
+//milimetros para metros
+float milimetros_metros (float milimetros){
+    return milimetros/1000;
+}
+
+//milimetros para centimetros
+float milimetros_centimetros (float milimetros){
+    return milimetros/10;
+}
