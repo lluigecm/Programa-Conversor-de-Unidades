80b90a0d42e1e4d031c57ab49956ee11d2ba30d3
CaioNatividade <caioprofissional22@gmail.com>
2024-12-22 01:14:10 -0300

    Adicionando mais conversões em eletricidade.c e ajustes de código

--- libs/eletricidade.c
+++ libs/eletricidade.c
@@ -4,6 +4,24 @@ double watts_para_kw(double watts) {
     return watts / 1000.0;
 }
 
+double kw_para_watts(double kw) {
+    return kw * 1000.0;
+}
+
 double kw_para_cv(double kw) {
-    return kw * 1.34102; // 1 kW = 1.34102 cv
+    return kw * 1.34102; // 1 kW = 1.34102 hp
+}
+
+double cv_para_kw(double cv) {
+    return cv / 1.34102;
+}
+
+double watts_para_cv(double watts) {
+    double kw = watts_para_kw(watts);
+    return kw_para_cv(kw);
+}
+
+double cv_para_watts(double cv) {
+    double kw = cv_para_kw(cv);
+    return kw_para_watts(kw);
 }
